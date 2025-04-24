21fcd11197eb2dffa2b8ea30c4e8e3239c7be0fe
drh-14 <darrenh1134@gmail.com>
2025-04-24 19:56:41 -0400

    finished recommendPieces endpoint

--- src/app/api/recommendPieces/route.ts
+++ src/app/api/recommendPieces/route.ts
@@ -3,14 +3,15 @@ import { Pinecone } from '@pinecone-database/pinecone'
 
 export default async function POST(req: NextRequest){
     const pc = new Pinecone({apiKey: process.env.PINECONE_KEY!});
-    const index = pc.index("semanticSearch").namespace("pieces");
+    const namespace = pc.index("music-semantic-search", "https://music-semantic-search-v3ofc6l.svc.aped-4627-b74a.pinecone.io").namespace("pieces");
     const {composers, instruments, characteristics} = await req.json();
     const query = `Pieces with composers: ${composers}, instruments: ${instruments}, characteristics: ${characteristics}`;
-    // search and rerank, todo
-    const results = await index.searchRecords({
+    const results = await namespace.searchRecords({
         query: {
           topK: 10,
           inputs: { text: query },
         },
-      });      
+        fields: ['title', 'composer', 'link']
+      });
+      return NextResponse.json(results.result.hits.map(elem => elem.fields)); 
 }
