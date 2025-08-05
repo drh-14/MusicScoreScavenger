fcf63dcaa07c906df92993a6b8bdbf1f79d68623
drh-14 <darrenh1134@gmail.com>
2025-08-05 19:49:49 -0400

    removed navbar from homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,16 +6,15 @@ export default function Home() {
   const router = useRouter();
   return (
     <div className = 'flex flex-col items-center gap-20 font-sans'>
-      <Navbar></Navbar>
       <div className = 'flex gap-8 flex-row-reverse w-full mr-20'>
       </div>
-      <div className = ' flex flex-col items-center gap-30 text-5xl'>
+      <div className = ' flex flex-col items-center gap-30 text-4xl'>
         Introducing MusicScoreScavenger
       </div>
-      <div className = 'text-4xl'>
+      <div className = 'text-3xl'>
         Your Personalized Classical Music Recommender
       </div>
-      <div className = 'text-4xl'>Find pieces tailored to your preferences and similar to your favorite pieces</div>
+      <div className = 'text-2xl'>Find pieces tailored to your preferences and similar to your favorite pieces</div>
       <Button onClick = {() => router.push('/questionnaire') } variant = 'contained'>Get Started</Button>
     </div>
   );
