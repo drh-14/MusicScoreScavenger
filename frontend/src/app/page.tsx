1ba13de692d9d249a5ec51a89329962217a6762f
drh-14 <darrenh1134@gmail.com>
2025-11-19 17:16:07 -0500

    separated code into frontend and backend folders

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,17 +1,14 @@
 'use client'
 import {Button} from '@mui/material';
 import { useRouter } from 'next/navigation';
+import Navbar from '../components/Navbar';
 export default function Home() {
   const router = useRouter();
   return (
     <div className = 'flex flex-col items-center gap-20 font-sans'>
-      <div className = 'flex gap-8 flex-row-reverse w-full mr-20'>
-      </div>
-      <div className = ' flex flex-col items-center gap-30 text-4xl'>
-        Introducing MusicScoreScavenger
-      </div>
-      <div className = 'text-3xl'>
-        Your Personalized Classical Music Recommender
+       <Navbar></Navbar>
+      <div className = 'text-5xl'>
+        Your key to finding new classical pieces.
       </div>
       <div className = 'text-2xl'>Find pieces tailored to your preferences and similar to your favorite pieces</div>
       <Button onClick = {() => router.push('/questionnaire') } variant = 'contained'>Get Started</Button>
