dc11759632f4d6bb0fda85b8390fdde6ed253887
drh-14 <darrenh1134@gmail.com>
2025-11-23 13:49:07 -0500

    changes to landing page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,17 +1,22 @@
-'use client'
-import {Button} from '@mui/material';
-import { useRouter } from 'next/navigation';
-import Navbar from '../components/Navbar';
+import { Button } from '@mui/material';
+import Link from 'next/link';
 export default function Home() {
-  const router = useRouter();
   return (
-    <div className = 'flex flex-col items-center gap-20 font-sans'>
-       <Navbar></Navbar>
-      <div className = 'text-5xl'>
-        Your key to finding new classical pieces.
+    <div className='flex flex-col items-center gap-40 font-sans'>
+      <div className = 'inline-block w-full p-6 bg-sky-400'>
+        <span className = 'text-4xl text-white'>MusicScoreScavenger</span>
+        <div className ='inline float-right'>
+          <div className = 'flex gap-8 w-full bg-sky-400 pr-2'>
+            <Button variant = 'contained'>Log in</Button>
+            <Button variant = 'contained'>Sign Up</Button>
+          </div>
+        </div>
       </div>
-      <div className = 'text-2xl'>Find pieces tailored to your preferences and similar to your favorite pieces</div>
-      <Button onClick = {() => router.push('/questionnaire') } variant = 'contained'>Get Started</Button>
+      <div className=' flex flex-col gap-24 items-center'>
+        <h1 className = 'text-5xl'>Your key to finding new classical pieces.</h1>
+        <div className='text-3xl'>Find new pieces tailored to your preferences.</div>
+      </div>
+      <Link href = '/questionnaire'><Button variant='contained' size = 'large'>Get Started</Button></Link>
     </div>
   );
 }
