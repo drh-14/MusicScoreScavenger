6410347d85a02975287fbbf6bfa25a51450d407b
drh-14 <darrenh1134@gmail.com>
2026-01-21 02:35:11 -0500

    vulnerability fix

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
     <div className='flex flex-col items-center gap-40 font-sans'>
       <Banner loggedIn = {false}></Banner>
       <div className=' flex flex-col gap-24 items-center'>
-        <h1 className = 'text-5xl'>Your key to finding new classical pieces. </h1>
+        <h1 className = 'text-5xl'>Your key to finding new classical pieces.</h1>
         <div className='text-3xl'>Find new pieces tailored to your preferences.</div>
       </div>
       <Link href = '/questionnaire'><CustomButton variant = "outlined">Get Started</CustomButton></Link>
